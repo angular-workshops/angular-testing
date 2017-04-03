333903946198d986622ea4730668620d6be04429
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-03 19:12:53 +0100

    task-3

--- src/app/exponential-strength.pipe/exponential-strength.pipe.isolated.spec.ts
+++ src/app/exponential-strength.pipe/exponential-strength.pipe.isolated.spec.ts
@@ -1,6 +1,16 @@
 import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
 describe('ExponentialStrengthPipe', () => {
-  it('should raise the value to the power of the exponent parameter');
-  it('should use "1" for the exponent if not provided');
+  let pipe: ExponentialStrengthPipe;
+  beforeEach(() => {
+    pipe = new ExponentialStrengthPipe();
+  });
+
+  it('should raise the value to the power of the exponent parameter', () => {
+    expect(pipe.transform(5, '2')).toEqual(25);
+  });
+
+  it('should use "1" for the exponent if not provided', () => {
+    expect(pipe.transform(5, undefined)).toEqual(5);
+  });
 });
