a4993fbbba47cba33395bb6ecb241d48f67e0286
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-03 19:09:53 +0100

    task-2

--- src/app/samples/orders/order.spec.ts
+++ src/app/samples/orders/order.spec.ts
@@ -10,7 +10,18 @@ describe('Order', () => {
   });
 
   describe('addItem(name, cost)', () => {
-    it('should not discount unpreferred customers');
-    it('should give preferred customers a 10% discount');
+    it('should not discount unpreferred customers', () => {
+      spyOn(customer, 'isPreferred').and.returnValue(false);
+      order.addItem('Angular', 100);
+      expect(customer.isPreferred).toHaveBeenCalled();
+      expect(order.items[0]).toEqual({ name: 'Angular', cost: 100 });
+    });
+
+    it('should give preferred customers a 10% discount', () => {
+      spyOn(customer, 'isPreferred').and.returnValue(true);
+      order.addItem('Angular', 100);
+      expect(customer.isPreferred).toHaveBeenCalled();
+      expect(order.items[0]).toEqual({ name: 'Angular', cost: 90 });
+    });
   });
 });
