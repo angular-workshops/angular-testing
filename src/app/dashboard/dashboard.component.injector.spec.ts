1351d91dd9ccd69f60b6a251e8888caf8bc3155a
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-03 19:37:13 +0100

    fix dashboard

--- src/app/dashboard/dashboard.component.injector.spec.ts
+++ src/app/dashboard/dashboard.component.injector.spec.ts
@@ -4,16 +4,29 @@ import { HeroService } from 'app/shared/hero.service';
 import { Hero } from 'app/shared/hero';
 import { DashboardComponent } from './dashboard.component';
 
+class MockHeroService {
+  constructor(private heroes: Hero[]) {}
+  getHeroes() {
+    return Promise.resolve(this.heroes);
+  }
+}
+
 describe('HeroService', () => {
   let injector: ReflectiveInjector;
   let heroes: Hero[];
 
   beforeEach(() => {
-    heroes = [ {id: 2, name: 'Rubberman', strength: 10, age: 15}, {id: 4, name: 'Dynama', strength: 4, age: 25} ];
-    injector = ReflectiveInjector.resolveAndCreate([ ]);
+    heroes = [
+      {id: 11, name: 'Mr. Nice', strength: 10, age: 15},
+      {id: 12, name: 'Narco', strength: 5, age: 20},
+      {id: 13, name: 'Bombasto', strength: 8, age: 23},
+      {id: 14, name: 'Celeritas', strength: 15, age: 30},
+      {id: 15, name: 'Magneta', strength: 2, age: 34}
+    ];
+    injector = ReflectiveInjector.resolveAndCreate([
+    ]);
   });
 
   describe('heroes', () => {
     it('should contain just the first three heroes after ngOnInit');
-  });
 });
