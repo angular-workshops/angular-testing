601f8387575d5678f40be58a6c3d65e4bf75e464
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-03 23:42:03 +0100

    task-10

--- src/app/exponential-strength.pipe/exponential-strength.pipe.hosted.spec.ts
+++ src/app/exponential-strength.pipe/exponential-strength.pipe.hosted.spec.ts
@@ -3,16 +3,27 @@ import { Component } from '@angular/core';
 import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
 @Component({
-  template: '...'
+  template: '<span>strength: {{ strength | exponentialStrength : exponent }}</span>'
 })
 class TestHostComponent {
+  strength = 5;
+  exponent = 2;
 }
 
 describe('exponential-strength.pipe', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [
+        ExponentialStrengthPipe,
+        TestHostComponent
+      ]
     });
   });
 
-  it('should show the exponentiated strength');
+  it('should show the exponentiated strength', () => {
+    const fixture = TestBed.createComponent(TestHostComponent);
+    const element = fixture.nativeElement;
+    fixture.detectChanges();
+    expect(element.textContent).toContain('25');
+  });
 });
